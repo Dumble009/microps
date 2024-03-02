0cd913b2130a1bd4f1b2daa62d3c43e479b3dcbc
daich-laptop-wsl <daich@laptop>
2024-03-02 17:00:56 +0900

    プロトコルのキューに積まれたデータをソフトウェア割り込みをトリガーとして吸い出すように変更

--- platform/linux/intr.c
+++ platform/linux/intr.c
@@ -7,6 +7,7 @@
 #include "platform.h"
 
 #include "util.h"
+#include "net.h"
 
 struct irq_entry
 {
@@ -88,6 +89,9 @@ intr_thread(void *arg)
         case SIGHUP:
             terminate = 1;
             break;
+        case SIGUSR1:
+            net_softirq_handler();
+            break;
         default:
             for (entry = irqs; entry; entry = entry->next)
             {
@@ -138,5 +142,6 @@ int intr_init(void)
     pthread_barrier_init(&barrier, NULL, 2);
     sigemptyset(&sigmask);
     sigaddset(&sigmask, SIGHUP);
+    sigaddset(&sigmask, SIGUSR1);
     return 0;
 }
