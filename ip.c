90dda80e0b0fe2ef695677c99266d4ce00c59819
daich-laptop-wsl <daich@laptop>
2024-03-06 10:03:15 +0900

    ICMPを登録IPに登録できるようにした

--- ip.c
+++ ip.c
@@ -26,11 +26,19 @@ struct ip_hdr
     uint8_t options[];
 };
 
+struct ip_protocol
+{
+    struct ip_protocol *next;
+    uint8_t type;
+    void (*handler)(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst, struct ip_iface *iface);
+};
+
 const ip_addr_t IP_ADDR_ANY = 0x00000000;       /* 0.0.0.0 */
 const ip_addr_t IP_ADDR_BROADCAST = 0xffffffff; /* 255.255.255.255 */
 
 /* NOTE: if you want to add/delete the entries after net_run(), you need to protect these lists with a mutex. */
 static struct ip_iface *ifaces;
+static struct ip_protocol *protocols;
 
 int ip_addr_pton(const char *p, ip_addr_t *n)
 {
@@ -160,6 +168,30 @@ ip_iface_select(ip_addr_t addr)
     return NULL;
 }
 
+/* NOTE: must not be call after net_run() */
+int ip_protocol_register(uint8_t type, void (*handler)(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst, struct ip_iface *iface))
+{
+    struct ip_protocol *entry;
+    for (entry = protocols; entry; entry = entry->next)
+    {
+        if (entry->type == type)
+        {
+            errorf("type %d is already registered.", type);
+            return -1;
+        }
+    }
+
+    entry = memory_alloc(sizeof(entry));
+    entry->type = type;
+    entry->handler = handler;
+    entry->next = protocols;
+    protocols = entry;
+
+    infof("registered, type=%u", entry->type);
+
+    return 0;
+}
+
 static void
 ip_input(const uint8_t *data, size_t len, struct net_device *dev)
 {
@@ -227,6 +259,16 @@ ip_input(const uint8_t *data, size_t len, struct net_device *dev)
 
     debugf("dev=%s, iface=%s, protocol=%u, total=%u", dev->name, ip_addr_ntop(iface->unicast, addr, sizeof(addr)), hdr->protocol, total);
     ip_dump(data, total);
+
+    struct ip_protocol *entry;
+    for (entry = protocols; entry; entry = entry->next)
+    {
+        if (entry->type == hdr->protocol)
+        {
+            entry->handler(data + hlen, total - hlen, hdr->src, hdr->dst, iface);
+            return;
+        }
+    }
 }
 
 static int
